33db29a2b5fd5d597e1ada1356bfe86d6a543503
Techcable <Techcable@techcable.net>
2022-04-24 22:17:55 -0700

    Basic setup for maven publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
     java
+    `maven-publish`
 }
+
+group = "net.techcable"
+version = "0.1.0"
 
 repositories {
     mavenCentral()
@@ -14,6 +18,23 @@ java {
     toolchain {
         // TODO: Remove module-info.java to make compatible with java 8
         languageVersion.set(JavaLanguageVersion.of(11))
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            artifactId = "tinyjson"
+
+            from(components["java"])
+        }
+    }
+
+    repositories {
+        maven {
+            name = "github-tinyjson"
+            url = uri("https://maven.pkg.github.com/Techcable/tinyjson")
+        }
     }
 }
 
